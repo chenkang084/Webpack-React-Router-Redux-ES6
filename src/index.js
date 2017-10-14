17586aad278ad82b3edd4aeea6fd6ed0b2f2b85c
Kang Kang1 Chen <chenkang1@Lenovo.com>
2017-10-14 14:15:05 +0800

    test framework

--- src/index.js
+++ src/index.js
@@ -100,7 +100,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Button } from "antd-mobile";
 // import "antd-mobile/dist/antd-mobile.css";
-import { TabBarExample } from "./components/Home/test";
+import { TabBarExample } from "./test";
 // import "./style.css";
 
 ReactDOM.render(
